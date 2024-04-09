cdb95bc58e2faf54579886bced10ab79266969e3
임수신 <iss0416@naver.com>
2024-04-09 16:01:11 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,10 +1,5 @@
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        for (int i = 0; i < my_string.length(); i++) {
-            if (my_string.substring(my_string.length() - 1 - i).equals(is_suffix)) {
-                return 1;
-            }
-        }
-        return 0;
+        return my_string.endsWith(is_suffix) ? 1 : 0;
     }
 }
