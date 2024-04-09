b720b97acf91d7a38977566fb980163985ea27bd
임수신 <iss0416@naver.com>
2024-04-09 15:49:05 +0900

    [level 0] Title: 접미사 배열, Time: 0.16 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class Solution {
     public String[] solution(String my_string) {
         int strSize = my_string.length();
@@ -6,16 +8,8 @@ class Solution {
         for (int i = 0; i < strSize; i++) {
             answer[i] = my_string.substring(strSize - 1 - i);
         }
-
-        for (int i = 0; i < answer.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if (answer[i].compareTo(answer[j]) < 0) {
-                    String temp = answer[i];
-                    answer[i] = answer[j];
-                    answer[j] = temp;
-                }
-            }
-        }
+        
+        Arrays.sort(answer);
         
         return answer;
     }
