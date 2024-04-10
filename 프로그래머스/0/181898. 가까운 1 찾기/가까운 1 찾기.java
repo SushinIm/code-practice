cccbbd30fb5572c38150fee32d61e6969478f16a
임수신 <iss0416@naver.com>
2024-04-10 21:24:52 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] arr, int idx) {
+        for (; idx < arr.length; idx++) {
+            if (arr[idx] == 1) {
+                return idx;
+            }
+        }
+        return -1;
+    }
+}
