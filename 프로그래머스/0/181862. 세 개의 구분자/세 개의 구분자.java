0da64089c46fb8f7835569ac0750452820cff804
임수신 <iss0416@naver.com>
2024-04-23 18:22:21 +0900

    [level 0] Title: 세 개의 구분자, Time: 56.74 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,7 +1,8 @@
+import java.util.Arrays;
+
 class Solution {
     public String[] solution(String myStr) {
-        String answer = myStr.replaceAll("a|b|c", " ").trim();
-        
-        return answer.length() == 0 ? new String[] { "EMPTY" } : answer.split("\\s+");
+        String[] arr = Arrays.stream(myStr.split("[abc]+")).filter(str -> !str.isEmpty()).toArray(String[]::new);
+        return arr.length == 0 ? new String[] { "EMPTY" } : arr;
     }
 }
