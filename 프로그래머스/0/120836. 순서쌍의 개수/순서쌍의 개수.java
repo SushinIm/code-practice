f446293a542e00d4b3efff59bd491708554210f8
임수신 <iss0416@naver.com>
2024-05-02 17:02:08 +0900

    [level 0] Title: 순서쌍의 개수, Time: 3.62 ms, Memory: 67.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        
+        int answer = 2;
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
