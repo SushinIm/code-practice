0a27d477072ed4e43b4b46751a700782cbe900a3
임수신 <iss0416@naver.com>
2024-05-17 17:04:28 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.35 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,15 +1,35 @@
+import java.util.*;
+
 class Solution {
     public int solution(String s) {
-        int before = 0;
         int answer = 0;
-        for (String num : s.split(" ")) {
-            if ("Z".equals(num)) {
-                answer -= before;
+        Stack<Integer> stack = new Stack<>();
+
+        for (String w : s.split(" ")) {
+            if (w.equals("Z")) {
+                stack.pop();
             } else {
-                before = Integer.parseInt(num);
-                answer += before;   
+                stack.push(Integer.parseInt(w));
             }
         }
+        for (int i : stack) {
+            answer += i;
+        }
         return answer;
+        
+        /*
+            스택 안쓰는 풀이
+            int before = 0;
+            int answer = 0;
+            for (String num : s.split(" ")) {
+                if ("Z".equals(num)) {
+                    answer -= before;
+                } else {
+                    before = Integer.parseInt(num);
+                    answer += before;   
+                }
+            }
+            return answer;
+        */
     }
 }
