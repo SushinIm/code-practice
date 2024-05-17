8b5515bcf33486c7030cef993b4430eaee2f0e54
임수신 <iss0416@naver.com>
2024-05-17 16:59:40 +0900

    [level 0] Title: 커피 심부름, Time: 0.04 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        for (String o : order) {
+            answer += o.contains("latte") ? 5000 : 4500;
+        }
+        return answer;
+    }
+}
