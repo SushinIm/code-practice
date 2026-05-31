1214550ff4919992da60364fb581265c41c2f3ee
임수신 <iss0416@naver.com>
2026-06-01 02:23:40 +0900

    [level 2] Title: 가장 큰 수, Time: 81.53 ms, Memory: 106 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = Arrays.stream(numbers)
+            .mapToObj(String::valueOf)
+            .sorted((a, b) -> (b + a).compareTo(a + b))
+            .collect(Collectors.joining());
+        
+        return answer.startsWith("0") ? "0" : answer;
+    }
+}
