1e4c913efa79b6567d1e243ec7aa4c499647c6f8
임수신 <iss0416@naver.com>
2026-06-01 01:38:33 +0900

    [level 2] Title: 타겟 넘버, Time: 4.48 ms, Memory: 61.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,26 @@
+class Solution {
+    int answer = 0;
+    int[] numbers;
+    int target;
+    
+    public int solution(int[] numbers, int target) {
+        this.numbers = numbers;
+        this.target = target;
+        
+        dfs(0, 0);
+        
+        return answer;
+    }
+    
+    public void dfs(int index, int sum) {
+        if (index == numbers.length) {
+            if (target == sum) {
+                answer++;
+            }
+            return;
+        }
+        
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
+    }
+}
